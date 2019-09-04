41d89b701160defc5f26bb55c392daf3da1b139f
翔 <297962650@qq.com>
2019-09-04 17:01:08 +0800

    11-实现注册表单的验证功能

--- components/user/registerForm.vue
+++ components/user/registerForm.vue
@@ -1,27 +1,27 @@
 <template>
-  <el-form :model="registerForm" ref="form" class="form">
-    <el-form-item class="form-item">
+  <el-form :model="registerForm" ref="form" :rules="rules" class="form">
+    <el-form-item class="form-item" prop="username">
       <el-input placeholder="用户名手机" v-model="registerForm.username"></el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="captcha">
       <!-- 文档地址：https://element.eleme.cn/#/zh-CN/component/input#fu-he-xing-shu-ru-kuang -->
-      <el-input placeholder="验证码">
+      <el-input placeholder="验证码" v-model="registerForm.captcha">
         <template slot="append">
           <el-button>发送验证码</el-button>
         </template>
       </el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="nickname">
       <el-input placeholder="你的名字" v-model="registerForm.nickname"></el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="password">
       <el-input placeholder="密码" type="password" v-model="registerForm.password"></el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="checkPassword">
       <el-input placeholder="确认密码" type="password" v-model="registerForm.checkPassword"></el-input>
     </el-form-item>
 
@@ -32,6 +32,19 @@
 <script>
 export default {
     data(){
+        // rule当前的规则，目前是空的
+        // value输入框的值
+        // callback是回调函数，必须要调用
+        const checkPassword = (rule,value,callback) => {
+            if(value === ''){
+                callback(new Error('请输入密码'))
+            }else if (value !== this.registerForm.password){
+                callback(new Error('两次输入密码不一致!'))
+            }else{
+                // 代表验证通过
+                callback();
+            }
+        }
         return{
             registerForm : {
                 username : '',
@@ -39,6 +52,23 @@ export default {
                 captcha : '',
                 password : '',
                 checkPassword :''
+            },
+             rules : {
+                username : [
+                    { required : true, message : '请输入用户名' ,trigger : 'blur' }
+                ],
+                nickname : [
+                    { required : true, message : '请输入昵称' ,trigger : 'blur' }
+                ],
+                captcha : [
+                    { required : true, message : '请输入验证码' ,trigger : 'blur' }
+                ],
+                password : [
+                    { required : true, message : '请输入密码' ,trigger : 'blur' }
+                ],
+                checkPassword : [
+                    { validator : checkPassword , tirgger : 'blur' }
+                ]
             }
         }
     }
