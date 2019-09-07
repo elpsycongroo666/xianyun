bc3774fc56fd7199932bd44b84ac87bdef940e84
翔 <297962650@qq.com>
2019-09-07 16:09:58 +0800

    飞机列表组件

---
+++ pages/air/flights.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="contianer">
+    <el-row type="flex" justify="space-between">
+      <!-- 顶部过滤列表 -->
+      <div class="flights-content">
+        <!-- 过滤条件 -->
+        <div></div>
+
+        <!-- 航班头部布局 -->
+        <filghtsHeader/>
+
+        <!-- 航班信息 -->
+        <filghtsList/>
+      </div>
+
+      <!-- 侧边栏 -->
+      <div class="aside">
+        <!-- 侧边栏组件 -->
+      </div>
+    </el-row>
+  </section>
+</template>
+
+<script>
+import filghtsHeader from '@/components/air/filghtsheader.vue'
+import filghtsList from '@/components/air/filghtslist.vue'
+export default {
+  data() {
+    return {};
+  },
+  components : {
+    filghtsHeader,filghtsList
+  }
+};
+</script>
+
+<style scoped lang="less">
+.contianer {
+  width: 1000px;
+  margin: 20px auto;
+}
+
+.flights-content {
+  width: 745px;
+  font-size: 14px;
+}
+
+.aside {
+  width: 240px;
+}
+</style>
